7449c02b927637254305550cb3419e91892a288c
uytildojey-858579 <david.molina95@proton.me>
2025-06-09 21:01:58 +0000

    Update lancer_wazuh_merci_dieu.sh

--- lancer_wazuh_merci_dieu.sh
+++ lancer_wazuh_merci_dieu.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-
-cd ./wazuh-docker/single-node
-docker-compose -f docker-compose.yml up -d
-
-echo "  → Utilisateur : admin"
-echo "  → Mot de passe : SecretPassword"
-echo "  → Accès : http://localhost:5601"
